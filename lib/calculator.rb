e4ae62e5737556d0ac331102a4af5a5c6cade964
Marcos Serpa <marcosserpa@gmail.com>
2017-11-21 11:32:25 -0200

    Passes the previous tests (check_input method).

---
+++ lib/calculator.rb
@@ -0,0 +1,32 @@
+module Calculator
+  attr_reader :number_1, :number_2, :operator
+
+  def initialize
+    begin
+      puts 'Hi pseudo-jedi! Welcome to the Stranger Things Calculator. Enter 2 numbers followed by a basic operator (+, -, *, /), everything separated by a whitespace:'
+
+      input = gets.chomp
+      @number_1, @number_2, @operator = input.split
+    end until check_input(number_1.to_s, number_2.to_s, operator.to_s)
+  end
+
+  def check_input(n1, n2, op)
+    begin
+      case
+      when (n1 && n2).match(/[^\d.]+/) # I know that the logig 'OR' is not so clear to read, but it's cute! :)
+        puts 'You cannot operate non-digit characters, young man. You must provide 2 numbers and an operator!'
+        return false
+      when (n1 && n2).empty? # I know that the logig 'OR' is not so clear to read, but it's cute! :)
+        puts 'You must provide 2 numbers and an operator, young padawan!'
+        return false
+      when op.empty? || !op.gsub(/[^a-zA-Z0-9]/, '').empty?
+        puts 'You still must provide the operator, young man!'
+        return false
+      end
+    rescue StandardError => e
+      puts 'Sorry! Some error ocurred and was not possible to process the operation. Contact the dumb who code this.'
+    end
+
+    true
+  end
+end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,34 +1,44 @@
 require 'calculator'
 
-describe Calculator do
+class CalculatorFake
+  include Calculator
+
+  def initialize(n1 = '10', n2 = '10', op = '+')
+    @number_1, @number_2, @operator = n1, n2, op
+  end
+end
+
+describe CalculatorFake do
   describe '.check_input' do
+    subject(:calculator) { CalculatorFake.new }
+
     context 'given only whitespace' do
       it 'returns an error message' do
-        expect{ check_input('', '', '') }.to output("You must provide 2 numbers and an operator, young padawan!\n").to_stdout
+        expect{ calculator.check_input('', '', '') }.to output("You must provide 2 numbers and an operator, young padawan!\n").to_stdout
       end
     end
 
     context 'given only one number' do
       it 'returns an error message' do
-        expect{ check_input('1', '', '') }.to output("You must provide 2 numbers and an operator, young padawan!\n").to_stdout
+        expect{ calculator.check_input('1', '', '') }.to output("You must provide 2 numbers and an operator, young padawan!\n").to_stdout
       end
     end
 
     context 'given a non-digit character' do
       it 'returns an error message' do
-        expect{ check_input('3', 'a', '') }.to output("You cannot operate non-digit characters, young man. You must provide 2 numbers and an operator!\n").to_stdout
+        expect{ calculator.check_input('3', 'a', '') }.to output("You cannot operate non-digit characters, young man. You must provide 2 numbers and an operator!\n").to_stdout
       end
     end
 
     context 'given 2 non-digit character' do
       it 'returns an error message' do
-        expect{ check_input('a', 'b', '') }.to output("You cannot operate non-digit characters, young man. You must provide 2 numbers and an operator!\n").to_stdout
+        expect{ calculator.check_input('a', 'b', '') }.to output("You cannot operate non-digit characters, young man. You must provide 2 numbers and an operator!\n").to_stdout
       end
     end
 
     context 'given no operator' do
       it 'returns an error message' do
-        expect{ check_input('55', '2', '') }.to output("You still must provide the operator, young man!\n").to_stdout
+        expect{ calculator.check_input('55', '2', '') }.to output("You still must provide the operator, young man!\n").to_stdout
       end
     end
   end
